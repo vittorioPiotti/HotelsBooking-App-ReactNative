74b7b6cda40510a318be619730618367b31b5ead
vittorioPiotti <vittoriopiotti.vp@gmail.com>
2024-08-16 12:04:42 +0200

    Update Home.tsx

--- project/source/src/scripts/features/navigation/Home.tsx
+++ project/source/src/scripts/features/navigation/Home.tsx
@@ -1,3 +1,16 @@
+/*
+ * Gestione Hotels App v1.0.0 (https://github.com/vittorioPiotti/Gestione-Hotels-App/releases/tag/1.0.0)
+ * Copyright 2024 Vittorio Piotti
+ * Licensed under GPL-3.0 (https://github.com/vittorioPiotti/Gestione-Hotels-App/blob/main/LICENSE.md)
+ */
+
+
+/*
+  React Native v0.74.0 (https://github.com/facebook/react-native/releases/tag/v0.74.0)
+  Copyright Facebook, Inc.
+  Licensed under MIT (https://github.com/facebook/react-native/blob/main/LICENSE)
+*/
+
 /**
  * @access public
  * @package src.scripts.features.navigation
